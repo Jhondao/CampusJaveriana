bd4cbfeb631bc0ee2250c280f26b51b9d228c04c
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2025-02-09 20:02:09 -0500

    Merge

--- components/EdificioCard.jsx
+++ components/EdificioCard.jsx
@@ -6,7 +6,7 @@ export function EdificioCard({ edificio }) {
     <Link href={`/${edificio.id}`} asChild>
       <View
         key={edificio.id}
-        className="flex-row bg-white shadow-lg p-4 rounded-2xl gap-4 mb-6 border border-gray-200 hover:bg-slate-100 hover:scale-105"
+        className="flex-row bg-white shadow-slate-500 p-4 rounded-2xl gap-4 mb-6 border border-gray-200 hover:bg-slate-100 hover:scale-105"
       >
         <Image
           source={{ uri: edificio.image }}
@@ -24,32 +24,3 @@ export function EdificioCard({ edificio }) {
     </Link>
   );
 }
-
-const styles = StyleSheet.create({
-  card: {
-    marginBottom: 20,
-    padding: 15,
-    backgroundColor: "#fff",
-    borderRadius: 8,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.5,
-    elevation: 5,
-  },
-  image: {
-    width: 107,
-    height: 147,
-    borderRadius: 10,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 10,
-  },
-  description: {
-    fontSize: 14,
-    marginTop: 5,
-    color: "#555",
-  },
-});
